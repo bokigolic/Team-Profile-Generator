88f08eca3fa9b45e301f83d3f57571f3520332fe
Bojan Golic <bokigolic32@gmail.com>
2021-10-31 23:16:33 -0400

    commmit

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,21 +3,21 @@ const Intern = require('../lib/Intern');
 
 // creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Jack', 007, 'jack@gmail.com', 'GW');
+    const intern = new Intern('Michael', 3223, 'michael@gmail.com', 'GW');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 // gets school from getSchool()
 test('gets employee school', () => {
-    const intern = new Intern('Bojan', 007, 'bokigoilic32@gmail.com', 'GW');
+    const intern = new Intern('Jack', 007, 'jack@gmail.com', 'GW');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const intern = new Intern('Bojan', 007, 'bokigoilic32@gmail.com', 'GW');
+    const intern = new Intern('Jack', 007, 'jack@gmail.com', 'GW');
 
     expect(intern.getRole()).toEqual("Intern");
 }); 
